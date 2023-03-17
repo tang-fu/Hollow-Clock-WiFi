7bfe53b191e7c9cfb534a430b5ae536ca8fa9a84
tang-fu <1224175892@qq.com>
2023-03-17 17:38:42 +0800

    自动校时程序

--- Codes/wifi_main_program/wifi_main_program.ino
+++ Codes/wifi_main_program/wifi_main_program.ino
@@ -3,14 +3,15 @@
 #include <WiFiUdp.h>
 // 4096 * 90 / 12 = 30720 default
 #define STEPS_PER_ROTATION 30743
-const char *ssid = "INPUT_YOUR_WIFI_ID";
-const char *password = "INPUT_YOUR_WIFI_PASSWORD";
+const char *ssid = "Mi 11";
+const char *password = "00000000";
 int currentHour = 0;
 int currentMinute = 0;
 //int currentSeconds = 0;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp1.aliyun.com", 60 * 60 * 8, 30 * 60 * 1000);  //偏移
-int delaytime = 1;
+int delaytime = 2;
+int num = 0;
 int port[4] = { 15, 13, 12, 14 };
 // sequence of stepper motor control
 int seq[8][4] = {
@@ -25,22 +26,42 @@ int seq[8][4] = {
 };
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   WiFi_Begin();
   timeClient.begin();
   pinMode(port[0], OUTPUT);
   pinMode(port[1], OUTPUT);
   pinMode(port[2], OUTPUT);
   pinMode(port[3], OUTPUT);
-  rotate(-20);         // for approach run
-  rotate(20);          // approach run without heavy load
-  Get_Current_Time();  //获取当前时间
+  rotate(-20);  // for approach run
+  rotate(20);   // approach run without heavy load
+  for (num = 0; num < 50; num++) {
+    timeClient.update();
+    currentHour = timeClient.getHours();
+    currentMinute = timeClient.getMinutes();
+    Serial.print("当前时间:");
+    Serial.print(currentHour);
+    Serial.print(":");
+    Serial.println(currentMinute);
+  }
   Time_Correction(currentHour);
 }
 
 void loop() {
-  stepper_run(currentMinute);
-  Get_Current_Time();
+  static long prev_min = 0, prev_pos = 0, pos = 0;
+  int min;
+  min = Get_Current_Time();
+  if (prev_min == min) {
+    return;
+  }
+  prev_min = min;
+  pos = (STEPS_PER_ROTATION * min) / 60;
+  rotate(-20);  // for approach run
+  rotate(20);   // approach run without heavy load
+  if (pos - prev_pos > 0) {
+    rotate(pos - prev_pos);
+  }
+  prev_pos = pos;
 }
 
 void WiFi_Begin() {
@@ -54,7 +75,7 @@ void WiFi_Begin() {
 
 int Get_Current_Time() {
   timeClient.update();
-  currentHour = timeClient.getHours();
+  //currentHour = timeClient.getHours();
   currentMinute = timeClient.getMinutes();
   //currentSeconds = timeClient.getSeconds();
   //Serial.println(currentMinute);//串口检查时间
@@ -65,28 +86,10 @@ int Get_Current_Time() {
   }
 }
 
-void stepper_run(long min){
-  static long prev_min = 0, prev_pos = 0;
-  static long pos;
-  if (prev_min == min) {
-    return;
-  }
-
-  prev_min = min;
-  pos = (STEPS_PER_ROTATION * min) / 60;
-  rotate(-20);  // for approach run
-  rotate(20);   // approach run without heavy load
-  if (pos - prev_pos > 0) {
-    rotate(pos - prev_pos);
-  }
-  prev_pos = pos;
-}
-
 void rotate(int step) {
   static int phase = 0;
-  int i, j;
+  int i, j, dt = 20;
   int delta = (step > 0) ? 1 : 7;
-  int dt = 20;
 
   step = (step > 0) ? step : -step;
   for (j = 0; j < step; j++) {
@@ -105,11 +108,21 @@ void rotate(int step) {
 
 void Time_Correction(int hour) {
   int i;
-  if(hour>12)
-  {
+  if (hour > 12) {
     hour -= 12;
   }
-  for (i = 0; i < hour; i++) {
-    stepper_run(60);
+  Serial.print("当前小时:");
+  Serial.println(hour);
+  for (i = 1; i <= hour; i++) {
+    int pos1 = 0, prev_pos1 = 0;
+    pos1 = (STEPS_PER_ROTATION * 60) / 60;
+    rotate(-20);  // for approach run
+    rotate(20);   // approach run without heavy load
+    if (pos1 - prev_pos1 > 0) {
+      rotate(pos1 - prev_pos1);
+    }
+    Serial.print("第");
+    Serial.print(i);
+    Serial.println("圈结束");
   }
 }
